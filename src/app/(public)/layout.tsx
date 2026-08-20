f1e727427a594707dbbed1f0458d994a1a2a4906
v-l1ma <viniciuslima957@hotmail.com>
2026-08-20 17:11:26 -0300

    fix: redirect

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,13 +1,3 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-
-export default async function PublicLayout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession(authOptions);
-
-  if (session) {
-    redirect("/dashboard");
-  }
-
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
